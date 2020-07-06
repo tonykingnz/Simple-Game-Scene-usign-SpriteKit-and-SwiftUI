1242747d14d136dcfb23a325f492f3753b389071
José Antônio Maria de Rezende Silva <joseant.silva@icloud.com>
2020-07-06 17:12:49 -0300

    Initial Commit

---
+++ Shared/SpriteKitGameSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteKitGameSwiftUIApp.swift
+//  Shared
+//
+//  Created by José Antônio Maria de Rezende Silva on 7/6/20.
+//
+
+import SwiftUI
+
+@main
+struct SpriteKitGameSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
